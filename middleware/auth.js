cacbcf53c9f7ab28b1d9966ae21f75f4d083700e
YurijM <y-myagkov@mail.ru>
2020-05-04 14:53:28 +0300

    Добавлен auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,13 @@
+export default function ({store, route, redirect}) {
+  if (route.path !== '/login' && !(/\/signup\/*/g.test(route.path))) {
+    if (route.path === '/profile') {
+      if (!store.getters['gambler/isSign'] && !store.getters['gambler/isAuth']) {
+        redirect('/login')
+      }
+    } else if (!store.getters['gambler/isAuth']) {
+      redirect('/login')
+    } else if (route.path === '/') {
+      redirect('/gambler')
+    }
+  }
+}
