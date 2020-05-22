a8c5950acece8a6fd3ee8b7f1c3273fc2980fe17
YurijM <y-myagkov@mail.ru>
2020-05-22 13:27:55 +0300

    Настройка для deploy

--- server/middleware/database.js
+++ server/middleware/database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   //connectionLimit: 10,
-  host: '89.223.122.221',
+  host: 'localhost',
   user: 'root',
   password: 'root',
   database: 'uefa2020',
