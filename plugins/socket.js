fddcca9bd28646143c22b08c259a6e110a2459eb
YurijM <y-myagkov@mail.ru>
2020-05-24 13:35:10 +0300

    Настройка deploy

--- plugins/socket.js
+++ plugins/socket.js
@@ -6,7 +6,7 @@ export default function({store}) {
   Vue.use(new VueSocketIO ({
     debug: false,
     //connection: 'http://' + (process.env.NOD_ENV === 'production' ? 'localhost:3000' : '192.168.1.64:3000'),
-    connection: 'http://89.223.122.221:3000',
+    connection: 'http://127.0.0.1:3000',
     vuex: {
       store,
       actionPrefix: 'socket_',
